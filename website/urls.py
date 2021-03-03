b2d6ee5a181b3efd0c7eaf1d877ba5741f28ab7a
rejina99 <mhrzn.rezyna07@gmail.com>
2021-03-03 15:10:15 +0545

    project

--- website/urls.py
+++ website/urls.py
@@ -7,8 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('squadfree.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
-
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
